560e64427f34bf27916568bfbfca32ff2fe2885a
clssvn <Yavladimir1201>
2020-03-24 02:32:37 +0100

    Initial check-in of module PRO_PL

---
+++ Onion.Infrastructure.MsSqlService/EntityFramework/StudentContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Onion.Domain.Entities;
+
+namespace Onion.Infrastructure.MsSqlService.EntityFramework
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=db-mssql;Initial Catalog=s17628;Integrated Security=True");
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Onion.Infrastructure.MsSqlService/Services/MsSqlService.cs
@@ -0,0 +1,34 @@
+﻿using Onion.Domain.Entities;
+using Onion.Domain.Services;
+using Onion.Infrastructure.MsSqlService.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Onion.Infrastructure.MsSqlService.Services
+{
+    public class MsSqlService : IStudentDbService
+    {
+        public bool EnrollStudent(Student newStudent, int semestr)
+        {
+            using (var context = new StudentContext())
+            {
+                context.Students.Add(newStudent);
+                context.SaveChanges();
+
+                return true;
+            }
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            using (var context = new StudentContext())
+            {
+                return context.Students.ToList<Student>();
+            }
+        }
+    }
+}
+
+
